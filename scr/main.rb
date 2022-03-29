a68c3e241f535c9e70d83b404b0dc2d2e8055941
Leslie-Gu <snowberg.prc@gmail.com>
2022-03-29 16:41:22 +1000

    doing sign-up feature

--- scr/main.rb
+++ scr/main.rb
@@ -27,29 +27,60 @@ def main_header
     puts "Welcome to Coder's Library!"
     prompt.select("How can I help you?", options)
 end
+#create a sign-up profile and 
+def create_profile(file)
+    system "clear"
+    hash = read_profile(file)
+    username = get_username(hash)
+    password = get_password #check password is right.
+end
 
+
+def read_profiles(file)
+    data = File.read(file)
+    return JSON.parse(data).sample
+end
+
+def get_username(hash)
+    loop = "continue"
+    print "Input your username  >> "
+    while loop != "stop"
+        username = gets.chomp!
+        if username.empty?
+            puts "Username can't be empty,please input again >>"
+        elsif hash.include? (username)
+            puts "The username has been taken, please try another one"
+        else
+            loop = "stop"
+        end
+    end
+    return username
+end
+
+def get_password
+    loop_2 = "continue"
+    print "Password >> "
+    while loop_2 != "stop"
+        password =  gets.chomp!
+        print "Re-enter your password >> "   
+        password_sec = gets.chomp!
+        if password != password_sec
+            print "Invalida password input, please try again >>"
+        else
+            loop_2 ="stop"
+        end
+    end
+    return password
+end
+
+def save_profile(user, file)
 
 
 def main 
     answer = main_header
-    
-    # if answer == "Today's Book"
-    #     system "clear"
-    #     logo
-    #     Books.new.random_book
-    #     puts " Would you like to borrow this book?"
-    #     prompt = TTY::Prompt.new
-    #     options = ["yes", "no", "back to the main page"]
-    #     prompt.select("Would you like to borrow the book?", options)
-    # elsif answer == "Find a book"
-    #     puts "Please input the book name:"
-    #     book_name = gets.chomp.Capitalize    
-    # elif answer == "Return a book"
-
-    # end
-
+    if answer == "Sign up"
+        user = create_profile('profiles.json')
+        p user
+    end
 end
 
-if ARGV.size == 0
-    main()
-end
---
+++ scr/spec/profiles_spec.rb
@@ -0,0 +1,6 @@
+
+require "./profiles.rb"
+
+describe Profile do
+    describe "#get_name"
+end
